f719c68dd2a287d11bdd3e98dc4f5bed8cb2a5c7
Devin Lauren Elder <laurenelder@fullsail.edu>
2013-06-06 14:13:04 -0400

    Added Images And Margins.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Devin "Lauren" Elder
+// ASD Term 1306
+// ASD Application
+// 06/03/2013
+
+$('#').on('pageinit', function() {
+	$('#').click(function() {
+
+	});
+	$('#').click(function() {
+
+	});
+	$('#').click(function() {
+
+	});
+	$('#').click(function() {
+
+	});
+});
+
+$('#').on('pageinit', function() {
+	$('#').click(function() {
+
+	});
+	$('#').click(function() {
+
+	});
+});
+
+$('#').on('pageinit', function() {
+	$('#').click(function() {
+
+	});
+	$('#').click(function() {
+
+	});
+});
